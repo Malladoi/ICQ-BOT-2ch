f5f2ad84ec46c69b8e144c50fb177d2bc0b47d24
Malladoi <iloveyoubutyoudontcare@gmail.com>
2018-09-18 23:16:37 +0300

    6

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def send_welcome(message):
     print(result)
 
 
-@bot.message_handler(func=lambda message: message.text == "Начать тест")
+@bot.message_handler(func=lambda message: message.text in ["Начать тест", "Еще разок?"])
 def getName(message):
     markup = types.ForceReply(selective=False)
     task = bot.reply_to(message, "Введите имя:", reply_markup=markup)
@@ -51,44 +51,36 @@ def test(message):
         a = random.randint(50, 200)
         result = None
         if a < 100:
+            markup = types.ReplyKeyboardMarkup(row_width=1)
+            itembtn1 = types.KeyboardButton("Еще разок?")
+            markup.add(itembtn1)
             task = bot.reply_to(message,
                                 "Ну что же, " + message.chat.username + " под псевдонимом " + message.text + "\nВаш результат: " + str(
-                                    a) + "\nДобро пожаловать в /soc/!")
-            markup = types.ReplyKeyboardMarkup(row_width=1)
-            itembtn1 = types.KeyboardButton("Начать тест")
-            itembtn2 = types.KeyboardButton("Не начинать тест")
-            markup.add(itembtn1, itembtn2)
-            task = bot.send_message(message, "Еще разок?", reply_markup=markup)
+                                    a) + "\nДобро пожаловать в /soc/!", reply_markup=markup)
             result = task.wait()
         elif a < 140:
+            markup = types.ReplyKeyboardMarkup(row_width=1)
+            itembtn1 = types.KeyboardButton("Еще разок?")
+            markup.add(itembtn1)
             task = bot.reply_to(message,
                                 "Ну что же, " + message.chat.username + " под псевдонимом " + message.text + "\nВаш результат: " + str(
-                                    a) + "\nПоздравляем! Вы нормальный человек.")
-            markup = types.ReplyKeyboardMarkup(row_width=1)
-            itembtn1 = types.KeyboardButton("Начать тест")
-            itembtn2 = types.KeyboardButton("Не начинать тест")
-            markup.add(itembtn1, itembtn2)
-            task = bot.send_message(message, "Еще разок?", reply_markup=markup)
+                                    a) + "\nПоздравляем! Вы нормальный человек.", reply_markup=markup)
             result = task.wait()
         elif a < 160:
+            markup = types.ReplyKeyboardMarkup(row_width=1)
+            itembtn1 = types.KeyboardButton("Еще разок?")
+            markup.add(itembtn1)
             task = bot.reply_to(message,
                                 "Ну что же, " + message.chat.username + " под псевдонимом " + message.text + "\nВаш результат: " + str(
-                                    a) + "\nВам можно доверить админку в конфе 2chby.")
-            markup = types.ReplyKeyboardMarkup(row_width=1)
-            itembtn1 = types.KeyboardButton("Начать тест")
-            itembtn2 = types.KeyboardButton("Не начинать тест")
-            markup.add(itembtn1, itembtn2)
-            task = bot.send_message(message, "Еще разок?", reply_markup=markup)
+                                    a) + "\nВам можно доверить админку в конфе 2chby.", reply_markup=markup)
             result = task.wait()
         else:
+            markup = types.ReplyKeyboardMarkup(row_width=1)
+            itembtn1 = types.KeyboardButton("Еще разок?")
+            markup.add(itembtn1)
             task = bot.reply_to(message,
                                 "Ну что же, " + message.chat.username + " под псевдонимом " + message.text + "\nВаш результат: " + str(
-                                    a) + "\nВышмат одним этажом выше.")
-            markup = types.ReplyKeyboardMarkup(row_width=1)
-            itembtn1 = types.KeyboardButton("Начать тест")
-            itembtn2 = types.KeyboardButton("Не начинать тест")
-            markup.add(itembtn1, itembtn2)
-            task = bot.send_message(message, "Еще разок?", reply_markup=markup)
+                                    a) + "\nВышмат одним этажом выше.", reply_markup=markup)
             result = task.wait()
     print(result)
 
