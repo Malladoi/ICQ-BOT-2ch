56a69c21f96acf4ca0be4ff1d0d635c0934d5ea0
Malladoi <iloveyoubutyoudontcare@gmail.com>
2018-09-18 22:59:59 +0300

    3

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ telebot.logger.setLevel(logging.CRITICAL)  # Outputs debug messages to console.
 bot = telebot.AsyncTeleBot(token + ":" + api_token)
 
 
-
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     markup = types.ReplyKeyboardMarkup(row_width=1)
@@ -31,6 +30,18 @@ def getName(message):
     result = task.wait()
     print(result)
 
+@bot.message_handler(func=lambda message: message.text == "Не начинать тест")
+def getFucked(message):
+    markup = types.ForceReply(selective=False)
+    markup = types.ReplyKeyboardMarkup(row_width=1)
+    itembtn1 = types.KeyboardButton("Начать тест")
+    itembtn2 = types.KeyboardButton("Не начинать тест")
+    markup.add(itembtn1, itembtn2)
+    # bot.send_message(message.chat.id, "Choose one letter:", reply_markup=markup)
+    task = bot.reply_to(message, "Че, зассцал?", reply_markup=markup)
+    result = task.wait()
+    print(result)
+
 @bot.message_handler(func=lambda message: True)
 def test(message):
     if message.reply_to_message.text == "Введите имя:" and message.text is not None:
@@ -38,18 +49,28 @@ def test(message):
         a = random.randint(50, 200)
         result = None
         if a < 100:
-            task = bot.reply_to(message, "Ну что же, " + message.chat.username + " под псевдонимом " + message.text + "\nВаш результат: " + str(a) + "\nДобро пожаловать в /soc/!", reply_markup=markup)
+            task = bot.reply_to(message,
+                                "Ну что же, " + message.chat.username + " под псевдонимом " + message.text + "\nВаш результат: " + str(
+                                    a) + "\nДобро пожаловать в /soc/!", reply_markup=markup)
             result = task.wait()
         elif a < 140:
-            task = bot.reply_to(message, "Ну что же, " + message.chat.username + " под псевдонимом " + message.text + "\nВаш результат: " + str(a) + "\nПоздравляем! Вы нормальный человек.", reply_markup=markup)
+            task = bot.reply_to(message,
+                                "Ну что же, " + message.chat.username + " под псевдонимом " + message.text + "\nВаш результат: " + str(
+                                    a) + "\nПоздравляем! Вы нормальный человек.", reply_markup=markup)
             result = task.wait()
         elif a < 160:
-            task = bot.reply_to(message, "Ну что же, " + message.chat.username + " под псевдонимом " + message.text + "\nВаш результат: " + str(a) + "\nВам можно доверить админку в конфе 2chby.", reply_markup=markup)
+            task = bot.reply_to(message,
+                                "Ну что же, " + message.chat.username + " под псевдонимом " + message.text + "\nВаш результат: " + str(
+                                    a) + "\nВам можно доверить админку в конфе 2chby.", reply_markup=markup)
             result = task.wait()
         else:
-            task = bot.reply_to(message, "Ну что же, " + message.chat.username + " под псевдонимом " + message.text + "\nВаш результат: " + str(a) + "\nВышмат одним этажом выше.", reply_markup=markup)
+            task = bot.reply_to(message,
+                                "Ну что же, " + message.chat.username + " под псевдонимом " + message.text + "\nВаш результат: " + str(
+                                    a) + "\nВышмат одним этажом выше.", reply_markup=markup)
             result = task.wait()
+
+
     print(result)
 
-bot.polling(none_stop=True)
 
+bot.polling(none_stop=True)
